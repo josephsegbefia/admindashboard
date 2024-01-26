4c652552dec3b875957221d76ead7b474027f727
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-01-26 20:01:18 +0000

    Finish all presentational components and start routes.js file

---
+++ src/components/SignOut.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SignOut = () => {
+  return (
+    <div>
+      <h1 className="title is-size-2">
+        You have been signed out!
+      </h1>
+    </div>
+  )
+}
+
+export default SignOut
