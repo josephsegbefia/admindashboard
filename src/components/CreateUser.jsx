02d718a6bde1bdf8f1d0f69ac1eb4fe8218ea1b4
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-01-26 19:18:07 +0000

    Complete createuser.jsx

---
+++ src/components/CreateUser.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const CreateUser = () => {
+  return (
+    <div>
+      <h1 className="title is-size-2">Create a new user</h1>
+        <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+                <input className="input" type="text" placeholder = "Joseph Segbefia" />
+            </div>
+        </div>
+
+        <div className="field">
+            <label className="label">Username</label>
+            <div className="control">
+                <input className="input" type="text" placeholder="elsegbefia@gmail.com" />
+            </div>
+        </div>
+
+        <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+                <input className="input" type="email" placeholder="Email" />
+            </div>
+        </div>
+
+        <div className="field">
+            <label className="label">Department</label>
+            <div className="control">
+                <div className="select">
+                    <select>
+                        <option>Select dropdown</option>
+                        <option>Management</option>
+                        <option>Logistics</option>
+                        <option>Marketing</option>
+                        <option>Maintenance</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div className="field is-grouped">
+            <div className="control">
+                <button className="button is-link">Create user</button>
+            </div>
+            <div className="control">
+                <button className="button is-link is-light">cancel</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CreateUser
