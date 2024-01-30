808bac3b83a6622a441278a711b92b9a94fbeb7a
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-01-30 11:07:43 +0000

    Add dynamic user data

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+import users from '../data/users.json';
+
+const getEditTime = () => {
+  const now = new Date();
+  return `${now.getHours()}:${now.getMinutes()}:${now.getMilliseconds()}`;
+};
 
 const Users = () => {
   return (
@@ -12,44 +20,18 @@ const Users = () => {
                     <th>Email</th>
                     <th>Department</th>
                     <th>Employment date</th>
+                    <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th>8</th>
-                    <td>Sally Appleseed</td>
-                    <td>sally@mail.com</td>
-                    <td>Management</td>
-                    <td>01 Jan 2014</td>
-                </tr>
-                <tr>
-                    <th>13</th>
-                    <td>Dave Davis</td>
-                    <td>dave.davis@mail.com</td>
-                    <td>Management</td>
-                    <td>07 Mar 2017</td>
-                </tr>
-                <tr>
-                    <th>11</th>
-                    <td>Alexis Macintosh</td>
-                    <td>amac@gmail.com</td>
-                    <td>Logistics</td>
-                    <td>24 Apr 2020</td>
-                </tr>
-                <tr>
-                    <th>2</th>
-                    <td>Mark Studdart</td>
-                    <td>mark@studdart.co.uk</td>
-                    <td>Marketing</td>
-                    <td>19 May 2016</td>
-                </tr>
-                <tr>
-                    <th>56</th>
-                    <td>Don Swanson</td>
-                    <td>d.swanson@goodbuildingcompany.com</td>
-                    <td>Maintenance</td>
-                    <td>13 Feb 2018</td>
-                </tr>
+              {
+                users.data.map(user => (
+                  <tr>
+                    <th>{user.id}</th>
+                    <td>{user.name}</td>
+                  </tr>
+                ))
+              }
             </tbody>
         </table>
     </div>
